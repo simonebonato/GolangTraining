c8e0b7be01c86556490c17e97e68b73a1bd4b075
simone.bonato <simone.bonato@fhnw.ch>
2024-10-23 09:59:30 +0200

    add: final v1 version

--- basic_web_app_playlist/v1/main.go
+++ basic_web_app_playlist/v1/main.go
@@ -5,7 +5,17 @@ import (
 	"net/http"
 )
 
+// the request is, well, the request that is made by the server, information that is sent to us
+// the response writer instead is a way the server can reply back to the server
+
+// ResponseWriter is an INTERFACE, because we can reply with different types of Writers
+// while the Reqest is a struct, since normally it has to follow a specific format
+
+// so functions that match this definition can be used to handle web requests in GO
+// there is a TYPE defining it called HandlerFunc, and we want to match it
+// we can call it however but the definition must be the same
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	// fprint let's us choose where we can write to
 	fmt.Fprint(w, "<h1>Welcome to Pippa's website!</h1>")
 }
 
