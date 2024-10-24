10b63213ecb2e5c7bda3d40213cf8f1edfc1283c
simone.bonato <simone.bonato@fhnw.ch>
2024-10-24 09:17:35 +0200

    add: v4 contact page

---
+++ basic_web_app_playlist/v4_contact_page/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to SimoPippa's website!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hi, this it the contact page...</h1>")
+	fmt.Fprint(w, "<p>To get in touch with me, send an email to: pippa@gmail.com")
+}
+
+func main() {
+	
+	http.HandleFunc("/", handlerFunc)
+
+	// we can register a new address and the HandlerFunc used to treat it
+	// in this case it's another HTML but in the future we can do different things :)
+	http.HandleFunc("/contact", contactHandler)
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
