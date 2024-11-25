a3e09bbf852ee242e80d641b7f8691191d56a6f9
simone.bonato <simone.bonato@fhnw.ch>
2024-11-25 10:52:46 +0100

    add: gophercise_18 display image in html (functioning)

--- gophercises/gophercise_18/img_transformer/main.go
+++ gophercises/gophercise_18/img_transformer/main.go
@@ -2,66 +2,89 @@ package main
 
 import (
 	"fmt"
+	"img_transformer/primitive"
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
 
+	// create a static directory used to serve files to the server
+	if _, err := os.Stat("static"); os.IsNotExist(err) {
+		os.Mkdir("static", 0755)
+	}
+
+	// html to submit a new picture
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, `
-            <form action="/upload" method="post" enctype="multipart/form-data">
-                <input type="file" name="image" accept="image/*">
-                <button type="submit">Upload</button>
-            </form>
-        `)
+	        <form action="/upload" method="post" enctype="multipart/form-data">
+	            <input type="file" name="image" accept="image/*">
+	            <button type="submit">Upload</button>
+	        </form>
+	    `)
 	})
 
 	e.POST("/upload", handleUpload)
-	e.GET("/display/:filename", handleDisplayImage)
+	e.Static("/static", "static")
 	e.Logger.Fatal(e.Start(":3001"))
 }
 
 func handleUpload(c echo.Context) error {
+	// load the image from the form
 	file, err := c.FormFile("image")
 	if err != nil {
 		fmt.Println("Error loading the image!")
 		return err
 	}
 
-	src, err := file.Open()
+	// open it into a file
+	f, err := file.Open()
 	if err != nil {
 		fmt.Println("Error opening the src!")
 		return err
 	}
-	defer src.Close()
+	defer f.Close()
 
-	dst, err := os.Create(filepath.Join("uploads", file.Filename))
+	// transform the image and copy the result
+	out, err := primitive.Transform(
+		f, 20,
+	)
 	if err != nil {
-		fmt.Println("Error finding the dst!")
+		panic(err)
+	}
+
+	// create a file in the static folder
+	timestamp := time.Now().UnixNano()
+	filename := fmt.Sprintf("static/out_%d.png", timestamp)
+
+	// Create destination file in the static folder
+	dst, err := os.Create(filename)
+	if err != nil {
 		return err
 	}
 	defer dst.Close()
 
-	if _, err = io.Copy(dst, src); err != nil {
-		fmt.Println("Error copying!")
-		return err
-	}
+	// copy the image from the io.Reader to the destination file
+	io.Copy(dst, out)
 
-	displayURL := fmt.Sprintf("/display/%s", file.Filename)
-	return c.HTML(
-		http.StatusOK, 
-		fmt.Sprintf("<p>File %s uploaded successfully. <a href='%s'>View image</a></p>", file.Filename, displayURL))
-}
+	// Prepare the HTML response
+	w := c.Response().Writer
+	w.Header().Set("Content-Type", "text/html")
 
-func handleDisplayImage(c echo.Context) error {
-	filename := c.Param("filename")
-    filepath := filepath.Join("uploads", filename)
+	fmt.Fprintln(w, "<html><body>")
+	fmt.Fprintln(w, "<h1>Transformed Image</h1>")
 
-    return c.File(filepath)
+	// Construct the image URL
+	imgURL := fmt.Sprintf("/static/out_%d.png", timestamp)
+	outString := fmt.Sprintf("<img src='%s' alt='Transformed Image'>", imgURL)
+	fmt.Fprint(w, outString)
+
+	fmt.Fprintln(w, "</body></html>")
+
+	return nil
 }
---
+++ gophercises/gophercise_18/img_transformer/primitive/primitive.go
@@ -0,0 +1,106 @@
+package primitive
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Defines the shapes used when transforming images
+type Mode int
+
+// Modes supported by the primitive package
+const (
+	ModeCombo Mode = iota
+	ModeTriangle
+	ModeRect
+	ModeEllipse
+	ModeCircle
+	ModeRotatedRect
+	ModeBeziers
+	ModeRotatedEllipse
+	ModePolygon
+)
+
+// Option for the transform function for the mode you want to use,
+// by default mode triangle will be used
+func WithMode(mode Mode) func() []string {
+	return func() []string {
+		return []string{"-m", fmt.Sprintf("%d", mode)}
+	}
+}
+
+// what I lack:
+// - what is an io.Reader and how to use it
+// - what is a buffer instead
+// - what is a tempFile for
+// - why are we using these things and not simply loading an image as in Python?
+
+// Transform will take the provided image as an io.Reader, apply the primitive transformation,
+// and return an io.Reader with the corresponding transformed image
+func Transform(image io.Reader, numShapes int, opts ...func() []string) (io.Reader, error) {
+	in, err := os.CreateTemp("", "in_*.png")
+	if err != nil {
+		return nil, err
+	}
+	// to make sure that the file is deleted at the end
+	defer os.Remove(in.Name())
+
+	// do the same for the output file
+	out, err := os.CreateTemp("", "out_*.png")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(out.Name())
+
+	// read image into input files
+	_, err = io.Copy(in, image)
+	if err != nil {
+		return nil, err
+	}
+	// we need to close the file for flushing
+	// layman terms: the system will finish writing all we need to write
+	// to the file, and we are sure in the next operation the file is good
+	in.Close()
+
+	// run primitive w/ -i in.Name() -o out.Name()
+	_, err = ExecPrimitive(in.Name(), out.Name(), numShapes, ModeCombo)
+	if err != nil {
+		return nil, err
+	}
+	// for flushing
+	out.Close()
+
+	// Reopen 'out' for reading
+	out, err = os.Open(out.Name())
+	if err != nil {
+		return nil, err
+	}
+	defer out.Close()
+
+	// Read from 'outputFile' into the buffer
+	buffer := new(bytes.Buffer)
+	_, err = io.Copy(buffer, out)
+	if err != nil {
+		return nil, err
+	}
+
+	// we return the buffer, because IT CAN become a Reader
+	// and we do not return the output file because it is deleted after the program runs
+	return buffer, nil
+}
+
+func ExecPrimitive(input_file string, output_file string, num_shapes int, mode Mode) (string, error) {
+	cmdString := fmt.Sprintf("-i %s -o %s -n %d -m %d", input_file, output_file, num_shapes, mode)
+	fmt.Println(cmdString)
+	cmd := exec.Command("primitive", strings.Fields(cmdString)...)
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(b))
+		panic(err)
+	}
+	return string(b), err
+}
